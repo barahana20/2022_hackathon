9251df57ff9ec729e19b479d57d226227be21c33
barahana13 <barahana13@naver.com>
2022-09-21 15:05:02 +0900

    change Required to Optional

--- dgsubway.subwaybixby/code/subwayOne.js
+++ dgsubway.subwaybixby/code/subwayOne.js
@@ -1,17 +1,25 @@
-
-export default function SubwayOne(subwayName) {
+import console from 'console';
+export default function subwayOne(subwayName) {
+  console.log(subwayName);
   function Time (hour,minute) {
     this.hour = hour;
     this.minute = minute;
   }
-
+  const leftDirectionArrivalTime = new Time(12,10);
+  const leftDirectionLeftTime = new Time(0,10);
+  const rightDirectionArrivalTime = new Time(12,15);
+  const rightDirectionLeftTime = new Time(0,5);
   // for prototype
-  let subwayResult = {
-    letleftDirectionArrivalTime : new Time(12,10),
-    leftDirectionLeftTime : new Time(0,10),
-    rightDirectionArrivalTime : new Time(12,15),
-    rightDirectionLeftTime : new Time(0,5)
+  // let subwayResult = {
+  //   leftDirectionArrivalTime : new Time(12,10),
+  //   leftDirectionLeftTime : new Time(0,10),
+  //   rightDirectionArrivalTime : new Time(12,15),
+  //   rightDirectionLeftTime : new Time(0,5)
+  // }
+  return {
+    leftDirectionArrivalTime: leftDirectionArrivalTime,
+    leftDirectionLeftTime: leftDirectionLeftTime,
+    rightDirectionArrivalTime: rightDirectionArrivalTime,
+    rightDirectionLeftTime: rightDirectionLeftTime
   }
-
-  return subwayResult
 }
