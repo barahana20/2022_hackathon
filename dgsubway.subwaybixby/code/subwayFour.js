1de864cf8628ff11bd633850291a725e33b0a9fe
barahana13 <barahana13@naver.com>
2022-09-22 02:52:16 +0900

    fix variable naming error

--- dgsubway.subwaybixby/code/subwayFour.js
+++ dgsubway.subwaybixby/code/subwayFour.js
@@ -2,14 +2,14 @@ import console from 'console';
 import http from 'http';
 export default function subwayOne(subwayName,subwayDir){
   
-  console.log('http://18.190.78.1:5001/four?subwayname='+subwayName['subwayName']+'&subwayDir='+subwayName['subwayDir']);
-  var response = http.getUrl('http://18.190.78.1:5001/four?subwayname='+subwayName['subwayName']+'&subwayDir='+subwayName['subwayDir'], { format: 'json' });
+  console.log('http://18.190.78.1:5001/four?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir']);
+  var response = http.getUrl('http://18.190.78.1:5001/four?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir'], { format: 'json' });
   console.log(response);
   const directionFirstTime = response['DirectionFirstTime'];
-  const directionLasTime = response['DirectionLasTime'];
+  const directionLastTime = response['DirectionLastTime'];
 
   return {
     directionFirstTime : directionFirstTime,
-    directionLasTime : directionLasTime,
+    directionLastTime : directionLastTime,
   }
 }
--- dgsubway.subwaybixby/code/subwayTwo.js
+++ dgsubway.subwaybixby/code/subwayTwo.js
@@ -2,8 +2,8 @@ import console from 'console';
 import http from 'http';
 export default function subwayOne(subwayName,subwayDir){
   
-  console.log('http://18.190.78.1:5001/two?subwayname='+subwayName['subwayName']+'&subwayDir='+subwayName['subwayDir']);
-  var response = http.getUrl('http://18.190.78.1:5001/two?subwayname='+subwayName['subwayName']+'&subwayDir='+subwayName['subwayDir'], { format: 'json' });
+  console.log('http://18.190.78.1:5001/two?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir']);
+  var response = http.getUrl('http://18.190.78.1:5001/two?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir'], { format: 'json' });
   console.log(response);
   const firstArrivalTime = response['leftDirectionArrivalTime'];
   const firstLeftTime = response['leftDirectionLeftTime'];
