81f28b5910086eb1ac4031501280e3a2890801dc
WhalesBob <ndw8200@naver.com>
2022-09-21 15:52:59 +0900

    modify js file(Time instance to Integer)

--- dgsubway.subwaybixby/code/subwayOne.js
+++ dgsubway.subwaybixby/code/subwayOne.js
@@ -1,14 +1,14 @@
 import console from 'console';
 export default function subwayOne(subwayName) {
   console.log(subwayName);
-  function Time (hour,minute) {
-    this.hour = hour;
-    this.minute = minute;
-  }
-  const leftDirectionArrivalTime = new Time(12,10);
-  const leftDirectionLeftTime = new Time(0,10);
-  const rightDirectionArrivalTime = new Time(12,15);
-  const rightDirectionLeftTime = new Time(0,5);
+  // function Time (hour,minute) {
+  //   this.hour = hour;
+  //   this.minute = minute;
+  // }
+  const leftDirectionArrivalTime = 1210
+  const leftDirectionLeftTime = 10
+  const rightDirectionArrivalTime = 1215
+  const rightDirectionLeftTime = 5
   // for prototype
   // let subwayResult = {
   //   leftDirectionArrivalTime : new Time(12,10),
