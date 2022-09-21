2eea54569363af9c48eb1c3366068dd34185c438
WhalesBob <ndw8200@naver.com>
2022-09-21 15:33:31 +0900

    recent try about testing

--- dgsubway.subwaybixby/code/subwayOne.js
+++ dgsubway.subwaybixby/code/subwayOne.js
@@ -1,16 +1,12 @@
 
 export default function SubwayOne(subwayName) {
-  function Time (hour,minute) {
-    this.hour = hour;
-    this.minute = minute;
-  }
-
+  
   // for prototype
   let subwayResult = {
-    letleftDirectionArrivalTime : new Time(12,10),
-    leftDirectionLeftTime : new Time(0,10),
-    rightDirectionArrivalTime : new Time(12,15),
-    rightDirectionLeftTime : new Time(0,5)
+    leftDirectionArrivalTime : 1210,
+    leftDirectionLeftTime : 10,
+    rightDirectionArrivalTime : 1215,
+    rightDirectionLeftTime : 5
   }
 
   return subwayResult
