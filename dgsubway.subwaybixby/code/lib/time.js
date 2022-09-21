9200b478f3c83af5223a4becbe69ac2251e2e3b7
WhalesBob <ndw8200@naver.com>
2022-09-21 09:54:13 +0900

    refactoring names and add some tests

---
+++ dgsubway.subwaybixby/code/lib/time.js
@@ -0,0 +1,4 @@
+function Time(hour,minute){
+  this.hour = hour;
+  this.minute = minute;
+}
--- dgsubway.subwaybixby/code/subwayOne.js
+++ dgsubway.subwaybixby/code/subwayOne.js
@@ -1,5 +1,12 @@
 module.exports.function = function subwayOne(subwayName){
 
-  SubwayOne1 subwayOne = 
+  // for prototype
+  let subwayResult = {
+    leftDirectionArrivalTime = new Time(12,10),
+    leftDirectionLeftTime = new Time(0,10),
+    rightDirectionArrivalTime = new Time(12,15),
+    rightDirectionLeftTime = new Time(0,5)
+  }
 
+  return subwayResult
 }
