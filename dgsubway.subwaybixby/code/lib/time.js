d6ef62c5322dbc9cdce4448aea50c85cc3adcd2f
barahana13 <barahana13@naver.com>
2022-09-21 14:22:36 +0900

    config error by ljh macbook

--- dgsubway.subwaybixby/code/lib/time.js
+++
@@ -1,4 +0,0 @@
-function Time(hour,minute){
-  this.hour = hour;
-  this.minute = minute;
-}
--- dgsubway.subwaybixby/code/subwayOne.js
+++ dgsubway.subwaybixby/code/subwayOne.js
@@ -1,11 +1,16 @@
-module.exports.function = function SubwayOne(subwayName){
+
+export default function SubwayOne(subwayName) {
+  function Time (hour,minute) {
+    this.hour = hour;
+    this.minute = minute;
+  }
 
   // for prototype
   let subwayResult = {
-    leftDirectionArrivalTime = new Time(12,10),
-    leftDirectionLeftTime = new Time(0,10),
-    rightDirectionArrivalTime = new Time(12,15),
-    rightDirectionLeftTime = new Time(0,5)
+    letleftDirectionArrivalTime : new Time(12,10),
+    leftDirectionLeftTime : new Time(0,10),
+    rightDirectionArrivalTime : new Time(12,15),
+    rightDirectionLeftTime : new Time(0,5)
   }
 
   return subwayResult
