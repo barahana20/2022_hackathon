54753c6a9d25d0e185546115cfdda3d9454fe828
barahana13 <barahana13@naver.com>
2022-09-21 18:39:06 +0900

    complete http

--- dgsubway.subwaybixby/code/subwayOne.js
+++ dgsubway.subwaybixby/code/subwayOne.js
@@ -1,24 +1,14 @@
 import console from 'console';
 import http from 'http';
 export default function subwayOne(subwayName) {
-  console.log(subwayName);
-  function Time (hour,minute) {
-    this.hour = hour;
-    this.minute = minute;
-  }
-  const leftDirectionArrivalTime = new Time(12,10);
-  const leftDirectionLeftTime = 10;
-  const rightDirectionArrivalTime = new Time(12,15);
-  const rightDirectionLeftTime = 5;
-  var response = http.getUrl('http://127.0.0.1:5001/two?subwayname=%EA%B0%81%EC%82%B0&subwaydir=%EC%83%81', { format: 'json' });
-  console.log ("response = " + response)
-  // for prototype
-  // let subwayResult = {
-  //   leftDirectionArrivalTime : new Time(12,10),
-  //   leftDirectionLeftTime : new Time(0,10),
-  //   rightDirectionArrivalTime : new Time(12,15),
-  //   rightDirectionLeftTime : new Time(0,5)
-  // }
+  
+  console.log('http://18.190.78.1:5001/one?subwayname='+subwayName.subwayName)
+  var response = http.getUrl('http://18.190.78.1:5001/one?subwayname='+subwayName.subwayName, { format: 'json' });
+  const leftDirectionArrivalTime = response['leftDirectionArrivalTime'];
+  const leftDirectionLeftTime = response['leftDirectionLeftTime'];
+  const rightDirectionArrivalTime = response['rightDirectionArrivalTime'];
+  const rightDirectionLeftTime = response['rightDirectionLeftTime'];
+
   return {
     leftDirectionArrivalTime: leftDirectionArrivalTime,
     leftDirectionLeftTime: leftDirectionLeftTime,
