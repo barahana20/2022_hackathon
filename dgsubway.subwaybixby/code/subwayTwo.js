c0dbd5ed576bcaf713024ba655689d1466d26296
WhalesBob <ndw8200@naver.com>
2022-09-22 21:54:53 +0900

    recent trying about subwayResult2(get one's out property from input value)

--- dgsubway.subwaybixby/code/subwayTwo.js
+++ dgsubway.subwaybixby/code/subwayTwo.js
@@ -3,6 +3,7 @@ import http from 'http';
 export default function subwayTwo(subwayName,subwayDir){
   
   console.log('http://18.190.78.1:5001/two?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir']);
+  console.log(subwayDir)
   var response = http.getUrl('http://18.190.78.1:5001/two?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir'], { format: 'json' });
   console.log(response);
   const firstArrivalTime = response['leftDirectionArrivalTime'];
@@ -15,6 +16,7 @@ export default function subwayTwo(subwayName,subwayDir){
     firstLeftTime : firstLeftTime,
     secondArrivalTime : secondArrivalTime,
     secondLeftTime : secondLeftTime,
+    subwayLine : 1,
     subwayDir : subwayDir
   }
 }
