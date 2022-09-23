ee6fc608a815b44d2601439afe3078f07b45a276
barahana13 <barahana13@naver.com>
2022-09-23 17:10:02 +0900

    d

--- backend/subway_method.py
+++ backend/subway_method.py
@@ -204,15 +204,14 @@ class SubwayMethod:
             else:
                 continue
             subwaytime = datetime.strptime(subwaytime, "%H:%M:%S")
+            lastTrainTime = subwaytime.strftime("%H:%M:%S")
             try:
-                if type(arrival_time.loc[str(int(i)-2)].values[0]) == str and ':' in arrival_time.loc[str(int(i)-2)].values[0]:
-                    beforeLastTrainTime = str(arrival_time.loc[str(int(i)-2)].values[0])
+                if type(arrival_time.loc[:, ::-1][str(int(i)-2)].values[0]) == str and ':' in arrival_time.loc[:, ::-1][str(int(i)-2)].values[0]:
+                    beforeLastTrainTime = str(arrival_time.loc[:, ::-1][str(int(i)-2)].values[0])
                 else:
                     continue
             except:
                 break
-            lastTrainTime = subwaytime.strftime("%H:%M:%S")
-            break
         return (firstTrainTime, lastTrainTime, beforeLastTrainTime, subwayLine)
         
     
