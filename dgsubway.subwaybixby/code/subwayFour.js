40238f83ecbdc31302f3b91f459cec62a25bf728
WhalesBob <ndw8200@naver.com>
2022-09-23 12:20:33 +0900

    debugging subwayTwo(almost compleete) and make view about 3,4

--- dgsubway.subwaybixby/code/subwayFour.js
+++ dgsubway.subwaybixby/code/subwayFour.js
@@ -1,15 +1,18 @@
 import console from 'console';
 import http from 'http';
-export default function subwayFour(subwayName,subwayDir){
+export default function subwayFour(subwayName,subwayDir,isFirstOrLast){
   
   console.log('http://18.190.78.1:5001/four?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir']);
   var response = http.getUrl('http://18.190.78.1:5001/four?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir'], { format: 'json' });
   console.log(response);
-  const directionFirstTime = response['DirectionFirstTime'];
-  const directionLastTime = response['DirectionLastTime'];
+  const directionFirstTime = response['directionFirstTime'];
+  const directionLastTime = response['directionLastTime'];
 
   return {
     directionFirstTime : directionFirstTime,
     directionLastTime : directionLastTime,
+    subwayLine : 1,
+    subwayDir : subwayName['subwayDir'],
+    isFirstOrLast : subwayName['isFirstOrLast']
   }
 }
--- dgsubway.subwaybixby/code/subwayTwo.js
+++ dgsubway.subwaybixby/code/subwayTwo.js
@@ -7,16 +7,16 @@ export default function subwayTwo(subwayName,subwayDir){
   console.log(subwayName)
   var response = http.getUrl('http://18.190.78.1:5001/two?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir'], { format: 'json' });
   console.log(response);
-  
-  const firstArrivalTime = response['leftDirectionArrivalTime'];
-  const firstLeftTime = response['leftDirectionLeftTime'];
-  const secondArrivalTime = response['rightDirectionArrivalTime'];
-  const secondLeftTime = response['rightDirectionLeftTime'];
 
-  console.log("1.firstArrivalTime : " + firstArrivalTime)
-  console.log("2.typeof(firstArrivalTime) : " + typeof(firstArrivalTime))
-  console.log("3.firstLeftTime : "+ firstLeftTime)
-  console.log("4.typeof(firstLeftTime) : "+ typeof(firstLeftTime))
+  let firstArrivalTime = response['firstArrivalTime'];
+  let firstLeftTime = response['firstLeftTime'];
+  let secondArrivalTime = response['secondArrivalTime'];
+  let secondLeftTime = response['secondLeftTime'];
+
+  console.log("l1.firstArrivalTime : " + firstArrivalTime)
+  console.log("l2.typeof(firstArrivalTime) : " + typeof(firstArrivalTime))
+  console.log("l3.firstLeftTime : "+ firstLeftTime)
+  console.log("l4.typeof(firstLeftTime) : "+ typeof(firstLeftTime))
 
   return {
     firstArrivalTime : firstArrivalTime,
