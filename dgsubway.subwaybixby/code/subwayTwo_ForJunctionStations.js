8111366f44a20d60758d2bb832111123876861d6
barahana13 <barahana13@naver.com>
2022-09-24 17:20:40 +0900

    subwayLine correct what appear undefined

--- dgsubway.subwaybixby/code/subwayTwo_ForJunctionStations.js
+++ dgsubway.subwaybixby/code/subwayTwo_ForJunctionStations.js
@@ -1,15 +1,14 @@
 import console from 'console';
 import http from 'http';
 export default function subwayTwo(junctionStations,subwayLine,subwayDir){
-
-  if(subwayLine == null){
-    subwayLine = findSubwayLine(subwayDir)
+  if(junctionStations['subwayLine'] == null){
+    subwayLine = findSubwayLine(junctionStations['subwayDir'])
   }
-  
-  console.log('http://18.190.78.1:5001/two_forjunctionstations?subwayname='+junctionStations['junctionStations']+'&subwaydir='+junctionStations['subwayDir']+'&subwayLine='+junctionStations['subwayLine']);
+
+  console.log('http://18.190.78.1:5001/two_forjunctionstations?subwayname='+junctionStations['junctionStations']+'&subwaydir='+junctionStations['subwayDir']+'&subwayLine='+subwayLine);
   console.log(junctionStations['subwayDir'])
   console.log(junctionStations)
-  var response = http.getUrl('http://18.190.78.1:5001/two_forjunctionstations?subwayname='+junctionStations['junctionStations']+'&subwaydir='+junctionStations['subwayDir']+'&subwayLine='+junctionStations['subwayLine'],{ format: 'json' });
+  var response = http.getUrl('http://18.190.78.1:5001/two_forjunctionstations?subwayname='+junctionStations['junctionStations']+'&subwaydir='+junctionStations['subwayDir']+'&subwayLine='+subwayLine,{ format: 'json' });
   console.log(response);
 
   let firstArrivalTime = response['firstArrivalTime'];
