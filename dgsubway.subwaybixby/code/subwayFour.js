923bead05e4176ea78f2707dee5f234db89fa8e9
barahana13 <barahana13@naver.com>
2022-09-25 11:06:29 +0900

    training data debug

--- dgsubway.subwaybixby/code/subwayFour.js
+++ dgsubway.subwaybixby/code/subwayFour.js
@@ -3,7 +3,6 @@ import http from 'http';
 export default function subwayFour(subwayName,subwayDir,isFirstOrLast){
   var url = 'http://18.190.78.1:5001/four?subwayname='+subwayName['subwayName']+'&subwaydir='+subwayName['subwayDir']
   var option = { format: 'json', cacheTime: 0}
-  console.log();
   var response = http.getUrl(url, option);
   console.log(response);
   const beforeLastTrainTime = response["beforeLastTrainTime"];
